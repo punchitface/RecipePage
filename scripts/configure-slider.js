b27705143b963123414ee5939e4077240de1b961
punchitface <punchitface@gmail.com>
2016-10-11 16:51:27 +0300

    Refactoring
    asdasd

---
+++ scripts/configure-slider.js
@@ -0,0 +1,7 @@
+var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    slidesPerView: 3,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    spaceBetween: 30
+});
